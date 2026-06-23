0e2e9b80c584b09d2cd8211ed8899466b18aedc9
rogerelizar-2026 <rogerelizar@gmail.com>
2026-06-23 17:34:59 -0300

    Update service worker cache version to v1.3.3

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // IMPORTANTE: Mude a versão para forçar atualização
-const CACHE_NAME = 'smart-wallet-v1.3.2';  // ← Aumente o número
+const CACHE_NAME = 'smart-wallet-v1.3.3';  // ← Aumente o número
 
 const ASSETS_TO_CACHE = [
     './',
