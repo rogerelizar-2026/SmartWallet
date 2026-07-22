acf26a0cd6dd6bd6e7bbb5cb5f496163cd967a3e
rogerelizar-2026 <rogerelizar@gmail.com>
2026-07-22 13:53:21 -0300

    Delete smart-finance-plus directory
    Signed-off-by: rogerelizar-2026 <rogerelizar@gmail.com>

--- smart-finance-plus/src/core/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * SmartFinance Core Module
- * Main entry point for the Smart Finance+ application
- */
-
-export { SmartFinance } from './SmartFinance.js';
-export { EventManager } from './EventManager.js';
-export { StateManager } from './StateManager.js';
